a73ef2148bbff93f2ddcfe6822d9c81cf8e87bcd
shivamminus <dubeyshivam044@gmail.com>
2022-05-07 09:55:06 +0530

    elk integration sucessfull

--- backend/app.py
+++ backend/app.py
@@ -101,4 +101,4 @@ if __name__ == '__main__':
         db.create_all()
     except Exception as e:
         print("DB is not created!", e)
-    app.run(debug = True, host="127.0.0.1",port=5000)
+    app.run(debug = False, host="127.0.0.1",port=5000)
